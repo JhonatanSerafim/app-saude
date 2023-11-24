74d7f74e070eea5ff511300391bab980b1a986b2
Jhonatan Serafim <serafim@MacBook-Pro-de-suporte.local>
2023-11-24 14:53:39 -0300

    page view

---
+++ static/js/chunk-2d0e1f75.75df16c4.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e1f75"],{"7d4f":function(n,t,e){"use strict";e.r(t);var l=function(n,t){var e=t._c;return e("div",{staticStyle:{padding:"30px"}},[e("el-alert",{attrs:{closable:!1,title:"menu 1-2-1",type:"warning"}})],1)},a=[],u=e("2877"),c={},i=Object(u["a"])(c,l,a,!0,null,null,null);t["default"]=i.exports}}]);
